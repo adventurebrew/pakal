e08db4b31618886ca23de4ee147c566bebbbeb41
BLooperZ <blooperz@users.noreply.github.com>
2022-09-12 00:29:49 +0300

    add example formats

---
+++ pakal/examples/installer.py
@@ -0,0 +1,29 @@
+import io
+import os
+from typing import IO
+
+from pakal.archive import ArchiveIndex, SimpleArchive, SimpleEntry, make_opener
+from pakal.examples.common import read_uint32_le
+
+
+def read_index_entries(stream: IO[bytes]) -> ArchiveIndex[SimpleEntry]:
+    _unk = stream.read(3)
+    size = read_uint32_le(stream)
+    subs = stream.read(size).split(b'\r\n')[:-1]
+    index = {}
+    for i in subs:
+        size = read_uint32_le(stream)
+        offset = stream.tell()
+        index[os.path.basename(i).decode('ascii')] = (offset, size)
+        stream.seek(size, io.SEEK_CUR)
+    rest = stream.read()
+    assert not rest, rest
+    return index
+
+
+class WestwoodInstaller(SimpleArchive):
+    def _create_index(self) -> ArchiveIndex[SimpleEntry]:
+        return read_index_entries(self._stream)
+
+
+open = make_opener(WestwoodInstaller)
---
+++ pakal/examples/pakfile.py
@@ -0,0 +1,39 @@
+from itertools import takewhile
+from functools import partial
+
+from typing import Any, IO, Iterable, Sequence, Tuple
+
+from pakal.archive import ArchiveIndex, SimpleArchive, SimpleEntry, make_opener
+from pakal.examples.common import readcstr, read_uint32_le
+
+
+def read_index_entry(stream: IO[bytes]) -> Tuple[str, int]:
+    return readcstr(stream).decode(), read_uint32_le(stream)
+
+
+def before_offset(stream: IO[bytes], off: int, *args: Any) -> bool:
+    return stream.tell() < off
+
+
+def read_index_entries(stream: IO[bytes]) -> Tuple[Sequence[str], Sequence[int]]:
+    off = read_uint32_le(stream)
+    index_entries = iter(partial(read_index_entry, stream), ('', 0))
+    index_entries = takewhile(partial(before_offset, stream, off), index_entries)
+    names, offs = zip(*index_entries)
+    return names, (off,) + tuple(offs)
+
+
+def create_index_mapping(
+    names: Iterable[str],
+    offsets: Sequence[int],
+) -> ArchiveIndex[SimpleEntry]:
+    sizes = [(end - start) for start, end in zip(offsets, offsets[1:])]
+    return dict(zip(names, zip(offsets, sizes)))
+
+
+class PakFile(SimpleArchive):
+    def _create_index(self) -> ArchiveIndex[SimpleEntry]:
+        return create_index_mapping(*read_index_entries(self._stream))
+
+
+open = make_opener(PakFile)
